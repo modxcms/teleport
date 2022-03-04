818c8ce45f6228b2b3d69faaaac3f6831ad7ce6d
Mike Schell <mike@webprogramming.ca>
2022-03-04 13:57:00 -0500

    replace each() with foreach() for PHP 8 compatibility

--- src/Teleport/Action/Extract.php
+++ src/Teleport/Action/Extract.php
@@ -385,7 +385,7 @@ class Extract extends Action
                                     $fields = implode(', ', array_map(array($this->modx, 'escape'), array_keys($row)));
                                 }
                                 $values = array();
-                                while (list($key, $value) = each($row)) {
+                                foreach ($row as $key => $value) {
                                     switch (gettype($value)) {
                                         case 'string':
                                             $values[] = $this->modx->quote($value);
